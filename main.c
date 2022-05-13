f24ab09b8728db38bb39ca62ef863b1a5c835fd6
Nikita <tikhonov-na2003@mail.ru>
2022-05-13 20:52:12 +0700

    main.c

---
+++ main.c
@@ -0,0 +1,47 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int Spaces(const int str_lenght, char str[], int i) {
+  for (; i < str_lenght; i++) {
+    if (str[i] == ' ') {
+      continue;
+    } else {
+      break;
+    }
+  }
+  return i;
+}
+int main() {
+  const int str_lenght = 50;
+  char str[str_lenght];
+  fgets(str, str_lenght, stdin);
+  int i = 0;
+  i = Spaces(str_lenght, str, i);
+  if (i >= str_lenght) {
+    printf("\nError:Volume exceeded\n");
+    return 1;
+  }
+  const char circle[] = "circle";
+  for (int g = 0; g < 6; g++, i++) {
+    if (tolower(str[i]) == circle[g]) {
+      continue;
+    } else {
+      printf("\nError:Wrong text input\n");
+      return 2;
+    }
+  }
+  i = Spaces(str_lenght, str, i);
+  if (i >= str_lenght) {
+    printf("\nError:Volume exceeded\n");
+    return 1;
+  }
+  float x, y, r;
+  if (3 == sscanf(str + i, "(%f %f, %f)\n", &x, &y, &r)) {
+    printf("%s\n", circle);
+    printf(" x = %f y = %f R = %f\n\n", x, y, r);
+    printf("Correct output:(%s %.2f %.2f,%.2f)\n", circle, x, y, r);
+  } else {
+    printf("\nError:Incorrect input of coordinates\n");
+  }
+}
