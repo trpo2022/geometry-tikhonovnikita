f4592a21671d009b950f3191db5bcc7336e5d906
Nikita <tikhonov-na2003@mail.ru>
2022-05-14 12:07:13 +0700

    src

---
+++ src/libgeometry/function.h
@@ -0,0 +1,9 @@
+#pragma once
+#ifndef _FUNCTIONS_H_
+#define _FUNCTIONS_H_
+
+int Space(const int str_lenght, char str[], int i);
+float Square(float r);
+float Perimetr(float r);
+
+#endif  //  _FUNCTIONS_H_
